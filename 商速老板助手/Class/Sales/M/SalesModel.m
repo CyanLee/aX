2c0f7734102c255928f0ab244b2d6548c600801b
zhaoning <zhaoningdeemail@qq.com>
2018-05-05 10:07:14 +0800

    改门店ui布局，打入接口，并无数据

---
+++ 商速老板助手/Class/Sales/M/SalesModel.m
@@ -0,0 +1,20 @@
+//
+//  SalesModel.m
+//  商速老板助手
+//
+//  Created by 赵宁 on 2018/5/5.
+//  Copyright © 2018年 luyonghao. All rights reserved.
+//
+
+#import "SalesModel.h"
+
+@implementation SalesModel
+
++ (instancetype)define{
+    SalesModel *tmp = [[SalesModel alloc]init];
+    tmp.payAmt = @"暂无";
+    tmp.payTimes = @"暂无";
+    tmp.payType = @"暂无";
+    return tmp;
+}
+@end
